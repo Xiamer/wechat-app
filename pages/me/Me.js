211384131a1de79db4c19d2b63d01af01d574944
5032651@qq.com <5032651@qq.com>
2016-12-28 23:45:43 +0800

    加底部bar

---
+++ pages/me/Me.js
@@ -0,0 +1,14 @@
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World'
+  },
+  //事件处理函数
+  bindViewTap: function() {
+
+  },
+  onLoad: function () {
+    console.log('onLoad')
+
+  }
+})
